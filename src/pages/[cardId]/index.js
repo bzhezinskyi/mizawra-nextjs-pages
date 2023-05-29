98975d3863f9be1a6d36d17b522b337a19ac81a6
Bzhezinskyi <bzhezinskyiillia@gmail.com>
2023-05-29 22:06:14 +0300

    Update index.js

--- src/pages/[cardId]/index.js
+++ src/pages/[cardId]/index.js
@@ -1,6 +1,17 @@
 import { useRouter } from "next/router";
+import narrative from "@/narrative.json";
 
 export default function Narrative() {
   const router = useRouter();
-  return <>{router.query.cardId}</>;
+  const dataIndex = narrative.findIndex((item) => {
+    return item.id.toString() === router.query.cardId;
+  });
+  const data = narrative[dataIndex];
+
+  return (
+    <>
+      <h2>{data.title}</h2>
+      <p>{data.text}</p>
+    </>
+  );
 }
